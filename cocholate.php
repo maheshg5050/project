e6191d94783fe941773e94eb157fafab6999ae91
maheshg5050 <maheshg5050@gmail.com>
2014-09-24 10:55:36 +0530

    css

---
+++ cocholate.php
@@ -0,0 +1,197 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+</head>
+
+<body>
+<div class="wrapper">
+	<div class="logo">
+	<img class="logo_img" src="img/logo.png"/>
+	</div>
+	<div class="line_divider">
+	</div>
+	<div class="menu">
+		<div class="menu_text" style="margin-left:18px;">
+		HOME
+		</div>
+		
+		<div class="menu_text">
+		CHOCOLATE COLLECTIONS
+		</div>
+		
+		<div class="menu_text">
+		BESTSELLERS
+		</div>
+		
+		<div class="menu_text">
+		GIFT PACKS
+		</div>
+		
+		
+		<div class="menu_text">
+		SPECIAL OCCASSION
+		</div>
+		
+		<div class="menu_text">
+		ORDER ONLINE
+		</div>
+		
+		<div class="menu_text">
+		WHAT'S NEW
+		</div>
+		
+		<div class="menu_text">
+		ABOUT US
+		</div>
+	
+	</div>
+	
+		
+	<div class="slider" style="min-height:100px;">
+	<div class="main_divider">
+	</div>	
+		<h1>CHOCOLATE COLLECTIONS </h1>
+		
+		<div class="main_divider">
+	</div>	
+	</div>
+	
+	<div class="content">
+
+		<div class="content_panel" style="background:#000;height:650px;">
+			<div class="panel_one_fourth" style="background:#3a150d;">
+				<div class="img">
+				<img width="200px;" src="img/logo.png"/>
+				</div>
+				<div class="text">
+					<!--<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+					
+				<h3>MILK MODERNE !</h3>
+				<img style="width:150px;" src="img/design1.png"/>
+				<div class="text_inner_big_div" style="">Chocolicious.. is a delectable assortment of chocolates created with love and care at home.
+Using the purest of ingredients and a mixture of imaginative fillings and toppings,
+each piece is a tantalizing promise of lingering delight.
+Share it with someone you love or indulge in some on your own.
+The taste will always be well.. Chocolicious</div>
+				</div>
+			</div>
+			
+					<div class="panel_one_fourth" style="background:#;">
+				<div class="img">
+				<img width="200px;" src="img/logo.png"/>
+				</div>
+				<div class="text">
+					<!--<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+					
+				<h3>GREEK REVIVAL !</h3>
+				<img style="width:150px;" src="img/design1.png"/>
+				<div class="text_inner_big_div" style="">Chocolicious.. is a delectable assortment of chocolates created with love and care at home.
+Using the purest of ingredients and a mixture of imaginative fillings and toppings,
+each piece is a tantalizing promise of lingering delight.
+Share it with someone you love or indulge in some on your own.
+The taste will always be well.. Chocolicious</div>
+				</div>
+			</div>
+			
+			
+					<div class="panel_one_fourth" style="background:#c34626;">
+				<div class="img">
+				<img width="200px;" src="img/logo.png"/>
+				</div>
+				<div class="text">
+					<!--<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+					
+				<h3>REAL TIME !</h3>
+				<img style="width:150px;" src="img/design1.png"/>
+				<div class="text_inner_big_div" style="">Chocolicious.. is a delectable assortment of chocolates created with love and care at home.
+Using the purest of ingredients and a mixture of imaginative fillings and toppings,
+each piece is a tantalizing promise of lingering delight.
+Share it with someone you love or indulge in some on your own.
+The taste will always be well.. Chocolicious</div>
+				</div>
+			</div>
+			
+			
+					<div class="panel_one_fourth" style="background:#68452f;">
+				<div class="img">
+				<img width="200px;" src="img/logo.png"/>
+				</div>
+				<div class="text">
+					<!--<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+					
+				<h3>SALT TALK !</h3>
+				<img style="width:150px;" src="img/design1.png"/>
+				<div class="text_inner_big_div" style="">Chocolicious.. is a delectable assortment of chocolates created with love and care at home.
+Using the purest of ingredients and a mixture of imaginative fillings and toppings,
+each piece is a tantalizing promise of lingering delight.
+Share it with someone you love or indulge in some on your own.
+The taste will always be well.. Chocolicious</div>
+				</div>
+			</div>
+			
+
+		</div>
+		
+		<div class="content_desception">
+		&nbsp;
+
+		</div>
+	
+	</div>
+	
+	<div class="footer">
+	<div class="line_divider">
+	</div>
+		<div class="social_nw">
+			<div class="join_mail">
+			<p>Click here to join our mailing list </p>
+			</div>
+			
+			<div class="social_icon">
+			<div style="margin:15px 10px 0 10px;float:left">Join us on </div>
+			<div><img class="s_icon" width="40px" src="img/fb.png"/>
+			<img class="s_icon" width="40px" src="img/img.png"/>
+			</div>
+			</div>
+		</div>
+	<!--	
+		<div class="site_map">
+			<div class="site_map_panel">
+			CHOCOLATE COLLECTIONS
+			</div>
+			
+			<div class="site_map_panel">
+			BESTSELLERS
+			</div>
+			
+			<div class="site_map_panel">
+			GIFT PACKS
+			</div>
+			
+			<div class="site_map_panel">
+			SPECIAL OCCASSION
+			</div>
+			
+			<div class="site_map_panel">
+			WHAT'S NEW
+			</div>
+			
+		</div> -->
+		
+		<div class="copyright">
+		copyright &copy; 2014 Chocolicious
+		</div>
+	
+	</div>
+		
+
+</div>
+
+
+
+</body>
+
+
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,160 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+</head>
+
+<body>
+<div class="wrapper">
+	<div class="logo">
+	<img class="logo_img" src="img/logo.png"/>
+	</div>
+	<div class="line_divider">
+	</div>
+	<div class="menu">
+		<div class="menu_text" style="margin-left:18px;">
+		HOME
+		</div>
+		
+		<div class="menu_text">
+		CHOCOLATE COLLECTIONS
+		</div>
+		
+		<div class="menu_text">
+		BESTSELLERS
+		</div>
+		
+		<div class="menu_text">
+		GIFT PACKS
+		</div>
+		
+		
+		<div class="menu_text">
+		SPECIAL OCCASSION
+		</div>
+		
+		<div class="menu_text">
+		ORDER ONLINE
+		</div>
+		
+		<div class="menu_text">
+		WHAT'S NEW
+		</div>
+		
+		<div class="menu_text">
+		ABOUT US
+		</div>
+	
+	</div>
+	
+		
+	<div class="slider">
+	<div class="line_divider">
+	</div>
+	<img width="1000px" src="img/slider1.jpg"/>
+	</div>
+	
+	<div class="content">
+	<div class="line_divider">
+	</div>
+		<div class="content_panel">
+			<div class="panel_one_third">
+				<div class="img">
+				<img height="200px;" src="img/p1.jpg"/>
+				</div>
+				<div class="text">
+					<!--<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+					
+				Chocolate Fantasies
+				</div>
+			</div>
+			
+				<div class="panel_one_third">
+				<div class="img">
+				<img height="200px;" src="img/p2.jpg"/>
+				</div>
+				<div class="text">
+				<!--	<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+			
+			Catering to your Taste
+				</div>
+			</div>
+			
+				<div class="panel_one_third">
+				<div class="img">
+				<img height="200px;" src="img/p3.jpg"/>
+				</div>
+				<div class="text">
+				<!--<img style="width:50px;margin:10px" src="img/more.png"/><br>-->
+			Gift packs for all occasions <br> 
+				
+				</div>
+			</div>
+		</div>
+		
+		<div class="content_desception">
+		
+		<h1>Chocolicious</h1>
+<h2>Chocolicious.. is a delectable assortment of chocolates created with love and care at home.<br>
+ Using the purest of ingredients and a mixture of imaginative fillings and toppings,<br>
+ each piece is a tantalizing promise of lingering delight.<br> Share it with someone you love or indulge in some on your own.<br>
+ The taste will always be well.. Chocolicious </h2>
+		</div>
+	
+	</div>
+	
+	<div class="footer">
+	<div class="line_divider">
+	</div>
+		<div class="social_nw">
+			<div class="join_mail">
+			<p>Click here to join our mailing list </p>
+			</div>
+			
+			<div class="social_icon">
+			<div style="margin:15px 10px 0 10px;float:left">Join us on </div>
+			<div><img class="s_icon" width="40px" src="img/fb.png"/>
+			<img class="s_icon" width="40px" src="img/img.png"/>
+			</div>
+			</div>
+		</div>
+	<!--	
+		<div class="site_map">
+			<div class="site_map_panel">
+			CHOCOLATE COLLECTIONS
+			</div>
+			
+			<div class="site_map_panel">
+			BESTSELLERS
+			</div>
+			
+			<div class="site_map_panel">
+			GIFT PACKS
+			</div>
+			
+			<div class="site_map_panel">
+			SPECIAL OCCASSION
+			</div>
+			
+			<div class="site_map_panel">
+			WHAT'S NEW
+			</div>
+			
+		</div> -->
+		
+		<div class="copyright">
+		copyright &copy; 2014 Chocolicious
+		</div>
+	
+	</div>
+		
+
+</div>
+
+
+
+</body>
+
+
+
+</html>
+
